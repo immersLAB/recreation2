2cd01e5a2189a9a8b59db3553cde70d68712fd0c
anhtdoan <102318434+anhtdoan@users.noreply.github.com>
2022-08-11 14:49:29 -0700

    works

--- Assets/calibrationWithKabsch.cs
+++ Assets/calibrationWithKabsch.cs
@@ -27,13 +27,31 @@ public class calibrationWithKabsch : MonoBehaviour
         }
         if (Input.GetKeyDown("space"))
         {
+            Vector3 inCentroid = Vector3.zero; Vector3 refCentroid = Vector3.zero;
+            float inTotal = 0f, refTotal = 0f;
+            for (int i = 0; i < inPoints.Length; i++)
+            {
+                inCentroid += new Vector3(inPoints[i].position.x, inPoints[i].position.y, inPoints[i].position.z) * refPoints[i].w;
+                inTotal += refPoints[i].w;
+                refCentroid += new Vector3(refPoints[i].x, refPoints[i].y, refPoints[i].z) * refPoints[i].w;
+                refTotal += refPoints[i].w;
+            }
+            inCentroid /= inTotal;
+            refCentroid /= refTotal;
+            Vector3 vec = inCentroid - refCentroid;
+            
+            Calibration.transform.position = -vec;
+            for (int i = 0; i < inPoints.Length; i++)
+            {
+                points[i] = inPoints[i].position;
+            }
             Matrix4x4 kabschTransform = solver.SolveKabsch(points, refPoints);
             Calibration.transform.position = kabschTransform.MultiplyPoint(Vector3.zero);
             Calibration.transform.rotation = kabschTransform.rotation;
-            for (int i = 0; i < inPoints.Length; i++)
+            /*for (int i = 0; i < inPoints.Length; i++)
             {
                 inPoints[i].position = kabschTransform.MultiplyPoint(points[i]);
-            }
+            }*/
         }
     }
 }
