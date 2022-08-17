5bb653764f8467f1662dbb071360e2a97b08549a
anhtdoan <102318434+anhtdoan@users.noreply.github.com>
2022-08-16 17:07:53 -0700

    moved white probe so tool rigid body doesn't get moved by red probe collider
    .
    
    fixed issues with colliders/ mislaigned scene content

--- Assets/calibrationWithKabsch.cs
+++ Assets/calibrationWithKabsch.cs
@@ -7,6 +7,7 @@ public class calibrationWithKabsch : MonoBehaviour
     KabschSolver solver = new KabschSolver();
     public GameObject Calibration;
     public GameObject CalibrationProbe;
+    public GameObject Tool;
     //Set up the Input Points
     void Start()
     {
@@ -21,44 +22,22 @@ public class calibrationWithKabsch : MonoBehaviour
     //Calculate the Kabsch Transform and Apply it to the input points
     void Update()
     {
-        CalibrationProbe.GetComponent<MeshCollider>().enabled = false;
-        CalibrationProbe.GetComponent<BoxCollider>().enabled = false;
-        for (int i = 0; i < inPoints.Length; i++)
-        {
-            refPoints[i] = new Vector4(referencePoints[i].position.x, referencePoints[i].position.y, referencePoints[i].position.z, referencePoints[i].localScale.x);
-        }
-        if (Input.GetKeyDown("space"))
-        {
-            Vector3 inCentroid = Vector3.zero; Vector3 refCentroid = Vector3.zero;
-            float inTotal = 0f, refTotal = 0f;
-            for (int i = 0; i < inPoints.Length; i++)
-            {
-                inCentroid += new Vector3(inPoints[i].position.x, inPoints[i].position.y, inPoints[i].position.z) * refPoints[i].w;
-                inTotal += refPoints[i].w;
-                refCentroid += new Vector3(refPoints[i].x, refPoints[i].y, refPoints[i].z) * refPoints[i].w;
-                refTotal += refPoints[i].w;
-            }
-            inCentroid /= inTotal;
-            refCentroid /= refTotal;
-            Vector3 vec = inCentroid - refCentroid;
-
-            Calibration.transform.position = -vec;
-            for (int i = 0; i < inPoints.Length; i++)
-            {
-                points[i] = inPoints[i].position;
-            }
-            Matrix4x4 kabschTransform = solver.SolveKabsch(points, refPoints);
-            Calibration.transform.position = kabschTransform.MultiplyPoint(Vector3.zero);
-            Calibration.transform.rotation = kabschTransform.rotation;
-            /*for (int i = 0; i < inPoints.Length; i++)
-            {
-                inPoints[i].position = kabschTransform.MultiplyPoint(points[i]);
-            }*/
-        }
+        
+        
 
     }
     public void Calibrate()
     {
+        Debug.Log("Calibrate:");
+        CalibrationProbe.GetComponent<MeshCollider>().enabled = false;
+        CalibrationProbe.GetComponent<BoxCollider>().enabled = false;
+     
+        if (Tool != null)
+        {
+            Tool.GetComponent<MeshCollider>().enabled = true;
+            Tool.GetComponent<CapsuleCollider>().enabled = true;
+        }
+
         for (int i = 0; i < inPoints.Length; i++)
         {
             refPoints[i] = new Vector4(referencePoints[i].position.x, referencePoints[i].position.y, referencePoints[i].position.z, referencePoints[i].localScale.x);
@@ -89,6 +68,7 @@ public class calibrationWithKabsch : MonoBehaviour
         {
             inPoints[i].position = kabschTransform.MultiplyPoint(points[i]);
         }*/
+        //CalibrationProbe.SetActive(false);
 
     }
 }
